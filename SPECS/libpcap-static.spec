de9d6715068470aaa76ad1df400316e0bdd5bdaf
root <root@front_aut_client_new140.(none)>
2014-12-10 06:06:46 -0500

    add build fragroute static and needed files

---
+++ SPECS/libpcap-static.spec
@@ -0,0 +1,188 @@
+# $Id$
+# Authority: dag
+
+# Tag: rft
+# ExclusiveDist: el6
+
+Summary: System-independent interface for user-level packet capture
+Name: libpcap
+Epoch: 14
+Version: 1.4.0
+Release: 1%{?dist}
+Group: Development/Libraries
+License: BSD with advertising
+URL: http://www.tcpdump.org
+
+Packager: Dag Wieers <dag@wieers.com>
+Vendor: Dag Apt Repository, http://dag.wieers.com/apt/
+
+Source: http://www.tcpdump.org/release/%{name}-%{version}.tar.gz
+Patch1: libpcap-man.patch
+Patch2: libpcap-multilib.patch
+Patch3: libpcap-s390.patch
+
+BuildRequires: bison
+BuildRequires: bluez-libs-devel
+BuildRequires: flex
+BuildRequires: glibc-kernheaders >= 2.2.0
+
+%description
+Libpcap provides a portable framework for low-level network
+monitoring.  Libpcap can provide network statistics collection,
+security monitoring and network debugging.  Since almost every system
+vendor provides a different interface for packet capture, the libpcap
+authors created this system-independent API to ease in porting and to
+alleviate the need for several system-dependent packet capture modules
+in each application.
+
+Install libpcap if you need to do low-level network traffic monitoring
+on your network.
+
+%package devel
+Summary: Libraries and header files for the libpcap library
+Group: Development/Libraries
+Requires: %{name} = %{epoch}:%{version}-%{release}
+
+%description devel
+Libpcap provides a portable framework for low-level network
+monitoring.  Libpcap can provide network statistics collection,
+security monitoring and network debugging.  Since almost every system
+vendor provides a different interface for packet capture, the libpcap
+authors created this system-independent API to ease in porting and to
+alleviate the need for several system-dependent packet capture modules
+in each application.
+
+This package provides the libraries, include files, and other
+resources needed for developing libpcap applications.
+
+%prep
+%setup
+
+%patch1 -p1 -b .man
+%patch2 -p1 -b .multilib
+%patch3 -p1 -b .s390
+
+#sparc needs -fPIC
+%ifarch %{sparc}
+sed -i -e 's|-fpic|-fPIC|g' configure
+%endif
+
+%build
+export CFLAGS="%{optflags} -fno-strict-aliasing"
+%configure
+# --disable-static
+%{__make} %{?_smp_mflags}
+
+%install
+%{__rm} -rf %{buildroot}
+%{__make} install DESTDIR="%{buildroot}"
+
+%post -p /sbin/ldconfig
+
+%postun -p /sbin/ldconfig
+
+%files
+%defattr(-, root, root, 0755)
+%doc CHANGES CREDITS LICENSE README
+%doc %{_mandir}/man7/pcap*.7*
+%{_libdir}/libpcap.so.*
+#%exclude %{_libdir}/libpcap.a
+%{_libdir}/libpcap.a
+
+
+%files devel
+%defattr(-, root, root, 0755)
+%doc %{_mandir}/man1/pcap-config.1*
+%doc %{_mandir}/man3/pcap*.3*
+%doc %{_mandir}/man5/pcap*.5*
+%{_bindir}/pcap-config
+%{_includedir}/pcap*.h
+%{_includedir}/pcap/
+%{_libdir}/libpcap.so
+
+%changelog
+* Wed May 22 2013 Dag Wieers <dag@wieers.com> - 14:1.4.0-1
+- Updated to release 1.4.0
+
+* Tue Mar 26 2013 Michal Sekletar <msekleta@redhat.com> - 14:1.3.0-4
+- remove unused variable from pcap-config to prevent multilib conflicts
+- specfile cleanup
+
+* Thu Feb 14 2013 Fedora Release Engineering <rel-eng@lists.fedoraproject.org> - 14:1.3.0-3
+- Rebuilt for https://fedoraproject.org/wiki/Fedora_19_Mass_Rebuild
+
+* Thu Jul 19 2012 Fedora Release Engineering <rel-eng@lists.fedoraproject.org> - 14:1.3.0-2
+- Rebuilt for https://fedoraproject.org/wiki/Fedora_18_Mass_Rebuild
+
+* Wed Jun 13 2012 Michal Sekletar <msekleta@redhat.com> 14:1.3.0-1
+- Update to 1.3.0
+
+* Thu Jan 05 2012 Jan Synáček <jsynacek@redhat.com> 14:1.2.1-2
+- Rebuilt for GCC 4.7
+
+* Tue Jan 03 2012 Jan Synáček <jsynacek@redhat.com> 14:1.2.1-1
+- Update to 1.2.1
+- Drop unnecessary -fragment patch
+
+* Fri Dec 02 2011 Michal Sekletar <msekleta@redhat.com> 14:1.2.0-1
+- update to 1.2.0
+
+* Tue Sep 06 2011 Michal Sekletar <msekleta@redhat.com> 14:1.1.1-4
+- fix capture of fragmented ipv6 packets
+
+* Fri Apr 22 2011 Miroslav Lichvar <mlichvar@redhat.com> 14:1.1.1-3
+- ignore /sys/net/dev files on ENODEV (#693943)
+- drop ppp patch
+- compile with -fno-strict-aliasing
+
+* Tue Feb 08 2011 Fedora Release Engineering <rel-eng@lists.fedoraproject.org> - 14:1.1.1-2
+- Rebuilt for https://fedoraproject.org/wiki/Fedora_15_Mass_Rebuild
+
+* Tue Apr 06 2010 Miroslav Lichvar <mlichvar@redhat.com> 14:1.1.1-1
+- update to 1.1.1
+
+* Wed Dec 16 2009 Miroslav Lichvar <mlichvar@redhat.com> 14:1.0.0-5.20091201git117cb5
+- update to snapshot 20091201git117cb5
+
+* Sat Oct 17 2009 Dennis Gilmore <dennis@ausil.us> 14:1.0.0-4.20090922gite154e2
+- use -fPIC on sparc arches
+
+* Wed Sep 23 2009 Miroslav Lichvar <mlichvar@redhat.com> 14:1.0.0-3.20090922gite154e2
+- update to snapshot 20090922gite154e2
+- drop old soname
+
+* Fri Jul 24 2009 Fedora Release Engineering <rel-eng@lists.fedoraproject.org> - 14:1.0.0-2.20090716git6de2de
+- Rebuilt for https://fedoraproject.org/wiki/Fedora_12_Mass_Rebuild
+
+* Wed Jul 22 2009 Miroslav Lichvar <mlichvar@redhat.com> 14:1.0.0-1.20090716git6de2de
+- update to 1.0.0, git snapshot 20090716git6de2de
+
+* Wed Feb 25 2009 Fedora Release Engineering <rel-eng@lists.fedoraproject.org> - 14:0.9.8-4
+- Rebuilt for https://fedoraproject.org/wiki/Fedora_11_Mass_Rebuild
+
+* Fri Jun 27 2008 Miroslav Lichvar <mlichvar@redhat.com> 14:0.9.8-3
+- use CFLAGS when linking (#445682)
+
+* Tue Feb 19 2008 Fedora Release Engineering <rel-eng@fedoraproject.org> - 14:0.9.8-2
+- Autorebuild for GCC 4.3
+
+* Wed Oct 24 2007 Miroslav Lichvar <mlichvar@redhat.com> 14:0.9.8-1
+- update to 0.9.8
+
+* Wed Aug 22 2007 Miroslav Lichvar <mlichvar@redhat.com> 14:0.9.7-3
+- update license tag
+
+* Wed Jul 25 2007 Jesse Keating <jkeating@redhat.com> - 14:0.9.7-2
+- Rebuild for RH #249435
+
+* Tue Jul 24 2007 Miroslav Lichvar <mlichvar@redhat.com> 14:0.9.7-1
+- update to 0.9.7
+
+* Tue Jun 19 2007 Miroslav Lichvar <mlichvar@redhat.com> 14:0.9.6-1
+- update to 0.9.6
+
+* Tue Nov 28 2006 Miroslav Lichvar <mlichvar@redhat.com> 14:0.9.5-1
+- split from tcpdump package (#193657)
+- update to 0.9.5
+- don't package static library
+- maintain soname
---
+++ build_static.sh
@@ -0,0 +1,220 @@
+#!/bin/bash
+#########################
+# build fragroute on 
+# CentOs
+# from root
+#
+# version 0.1 <09.12.2014>
+# <weldpua2008@gmail.com>
+#
+#########################
+# initial versions of fragroute 
+# and libdnet placed into:
+# https://code.google.com/p/fragroute-ipv6/
+# and moved to	
+#	https://github.com/stsi/fragroute-ipv6, 
+#	https://github.com/stsi/libdnet-ipv6
+#
+###########################
+
+# variables for getopts
+# 
+BUILD_STATIC=true
+INSTALL_DEPENDENCES=true
+INSTALL_REPOSITORY=true
+GET_FILES_FROM_GIT=true
+##########
+GITHUB_PREFIX="https://raw.githubusercontent.com/weldpua2008/fragroute/master"
+FRAGROUTE_SPEC="${GITHUB_PREFIX}/SPECS/fragroute.spec"
+FRAGROUTE_STATIC_SPEC="${GITHUB_PREFIX}/SPECS/fragroute-static.spec"
+LIBDNET_SPEC="${GITHUB_PREFIX}/SPECS/libdnet.spec"
+LIBPCAP_SPEC="${GITHUB_PREFIX}/SPECS/libpcap-static.spec"
+
+SPEC_PATH=~/rpmbuild/SPECS
+SOURCES_PATH=~/rpmbuild/SOURCES
+RPMS_PATH=~/rpmbuild/RPMS/`arch`
+
+
+
+#FRAGROUTE_REPO="https://github.com/stsi/fragroute-ipv6"
+FRAGROUTE_REPO="https://github.com/weldpua2008/fragroute-ipv6.git"
+#LIBDNET_REPO="https://github.com/stsi/libdnet-ipv6"
+LIBDNET_REPO="https://github.com/weldpua2008/libdnet-ipv6.git"
+
+# variables
+FRAGROUTE_FILE_NAME=fragroute-1.2.6-ipv6.tar.gz
+LIBDNET_FILE_NAME=libdnet-1.12.tar.gz
+LIBPCAP_SOURCES_FILES_T="libpcap-1.4.0.tar.gz libpcap-man.patch libpcap-multilib.patch libpcap-s390.patch"
+
+# DST files
+LIBDNET_DST_FILE="${GITHUB_PREFIX}/SOURCES/${LIBDNET_FILE_NAME}"
+FRAGROUTE_DST_FILE="${GITHUB_PREFIX}/SOURCES/${FRAGROUTE_FILE_NAME}"
+LIBPCAP_SOURCES_FILES=( $LIBPCAP_SOURCES_FILES_T )
+
+# repository for all centos
+EPEL_CENTOS5="http://dl.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm"
+EPEL_CENTOS6="http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm"
+EPEL_CENTOS7="http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-1.noarch.rpm"
+
+
+while getopts ":s" opt; do
+  case $opt in
+    s)
+	echo "-s start to build static!" >&2
+	BUILD_STATIC=true	
+      ;;
+    d)
+	echo "-d start to build dinamic!" >&2
+	BUILD_STATIC=false
+	;;
+    n)
+	echo "-n not install dependencies"
+	INSTALL_DEPENDENCES=false
+	;;
+    r)
+	echo "-r not install repository"
+	INSTALL_REPOSITORY=false
+	;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+# added github.com to path
+for (( i = 0 ; i < ${#LIBPCAP_SOURCES_FILES[@]} ; i++ )) do
+    LIBPCAP_SOURCES_FILES[$i]=${GITHUB_PREFIX}/SOURCES/${LIBPCAP_SOURCES_FILES[$i]} 
+done
+
+if [ ${INSTALL_DEPENDENCES} == true ];then
+	yum groupinstall "Development Tools" -y 
+	yum install git -y
+	echo " install libpcap libevent-devel"
+	yum install libevent-devel -y
+	if [ $BUILD_STATIC == true ];then
+		yum install glibc-static.x86_64 bluez-libs-devel -y
+	else
+		yum install libpcap libevent-devel libpcap-devel -y
+	fi
+fi
+
+
+echo "install repository"
+if [ -f /etc/redhat-release  ] && [ $INSTALL_REPOSITORY == true ];then
+	echo ""
+	echo -n "install repository of EPEL for "	
+	 cat /etc/redhat-release|cut -d "." -f1 |grep 7 &> /dev/null
+        if [ $? -eq 0 ];then
+		echo -n "CentOs7"
+                wget ${EPEL_CENTOS7}
+                rpm -Uvh epel-release*.rpm
+                rm epel-release*.rpm
+        fi
+
+	cat /etc/redhat-release|cut -d "." -f1 |grep 6 &> /dev/null
+	if [ $? -eq 0 ];then
+		echo -n "CentOs6"
+		wget ${EPEL_CENTOS6}
+		rpm -Uvh epel-release*.rpm
+		rm epel-release*.rpm
+	fi
+
+	cat /etc/redhat-release|cut -d "." -f1 |grep 5 &> /dev/null
+        if [ $? -eq 0 ];then
+		echo -n "CentOs5"
+                wget ${EPEL_CENTOS5}
+                rpm -Uvh epel-release*.rpm
+                rm epel-release*.rpm
+        fi
+
+	echo  "..."
+fi
+
+
+echo " create rpmbuild stuff"
+mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
+
+cd ${SPEC_PATH}
+	echo "store spec files to ${SPEC_PATH}"
+	wget ${FRAGROUTE_SPEC} 
+	wget ${FRAGROUTE_STATIC_SPEC}
+	wget ${LIBDNET_SPEC}
+	wget ${LIBPCAP_SPEC}
+
+cd ${OLDPWD}
+cd ${SOURCES_PATH}
+	echo "get fragroute files from repositories"
+	filename=$(basename $FRAGROUTE_REPO)
+	filename=${filename%.*}
+	rm -rf ${filename} &> /dev/null
+	rm -rf ${FRAGROUTE_FILE_NAME%.*.*} &> /dev/null
+	rm -rf ${FRAGROUTE_FILE_NAME} &> /dev/null
+	git clone ${FRAGROUTE_REPO}
+	mv ${filename} ${FRAGROUTE_FILE_NAME%.*.*}
+	tar czf ${FRAGROUTE_FILE_NAME} ${FRAGROUTE_FILE_NAME%.*.*}
+
+	echo "get libdnet files from repositories"
+        filename=$(basename $LIBDNET_REPO)
+        filename=${filename%.*}
+	rm -rf ${filename} &> /dev/null
+	rm -rf ${LIBDNET_FILE_NAME%.*.*} &> /dev/null
+	rm -rf  ${LIBDNET_FILE_NAME} &> /dev/null
+
+
+        git clone ${LIBDNET_REPO}
+        mv ${filename} ${LIBDNET_FILE_NAME%.*.*}
+        tar czf ${LIBDNET_FILE_NAME} ${LIBDNET_FILE_NAME%.*.*}
+
+
+	echo "get libpcap for static build"
+	for (( i = 0 ; i < ${#LIBPCAP_SOURCES_FILES[@]} ; i++ )) do
+		  wget  ${LIBPCAP_SOURCES_FILES[$i]}
+	done
+
+
+
+cd ${OLDPWD}
+
+cd ${SPEC_PATH}
+	if [ ! -f  $(basename $LIBDNET_SPEC) ];then 
+		echo " there aren't ${SPEC_PATH}/$(basename $LIBDNET_SPEC), exiting..."
+		exit 1
+	fi
+	echo "build libdnet"
+	rpmbuild -ba $(basename $LIBDNET_SPEC)
+	rpm -Uvh ${RPMS_PATH}/libdnet-*.rpm
+
+
+	case "$BUILD_STATIC" in 
+		"true")
+			if [ ! -f $(basename $LIBPCAP_SPEC) ];then		
+				echo " there aren't ${SPEC_PATH}/$(basename $LIBPCAP_SPEC), exiting..."
+				exit 1
+			fi
+			echo "build libpcap-static"
+			rpmbuild -ba $(basename $LIBPCAP_SPEC)
+			rpm -Uvh ${RPMS_PATH}/libpcap*.rpm
+
+		 	if [ ! -f  $(basename  $FRAGROUTE_STATIC_SPEC) ];then
+                                echo " there aren't ${SPEC_PATH}/$(basename $FRAGROUTE_STATIC_SPEC), exiting..."
+                                exit 1
+                        fi
+			echo "build fragroute-static"
+			rpmbuild -ba $(basename $FRAGROUTE_STATIC_SPEC)
+
+		;;
+		"false")
+
+			if [ ! -f  $(basename  $FRAGROUTE_SPEC) ];then
+				echo " there aren't ${SPEC_PATH}/$(basename $FRAGROUTE_SPEC), exiting..."
+				exit 1
+			fi
+			echo "build fragroute"
+			rpmbuild -ba $(basename $FRAGROUTE_SPEC)
+		;;
+	esac
+
+cd ${OLDPWD}
+
+exit 0
